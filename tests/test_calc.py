bdfb65d785ed5b436c057410e02569fd20fe6193
Nadavda1 <nadavde2@gmail.com>
2025-02-06 09:56:45 +0200

    after fix

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -7,10 +7,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from calc import add
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+    assert add(7, 20) == 27
+
 
 
 
