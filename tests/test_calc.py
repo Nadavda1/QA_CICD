bba39038a57a7406dc92e2c1001e34c9d5bf93ab
Nadavda1 <nadavde2@gmail.com>
2025-02-06 09:31:22 +0200

    Initial commit

---
+++ tests/test_calc.py
@@ -0,0 +1,17 @@
+# tests/test_calc.py
+import sys
+import os
+
+# הוספת תיקיית האב ל־sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from calc import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+
+
