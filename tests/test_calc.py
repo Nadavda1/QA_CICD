052673b63d3b7c3a3cf3fe87ebbed8e5117f2230
Nadavda1 <nadavde2@gmail.com>
2025-02-19 15:25:13 +0200

    add_sub_tst

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,8 +5,9 @@ import os
 # הוספת תיקיית האב ל־sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from calc import add
 
+from calc import add
+from calc import sub
 
 def test_add():
     assert add(2, 3) == 5
@@ -14,6 +15,8 @@ def test_add():
     assert add(0, 0) == 0
     assert add(7, 20) == 27
 
+def test_sub():
+    assert sub(2,2) == 0
 
 
 
